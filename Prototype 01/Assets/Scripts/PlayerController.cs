994ce8cfef4b651ec20e14260cc918ef8757736a
PauCape <pau.capella@hotmail.es>
2023-01-08 18:29:47 +0100

    Controles basicos de movimiento implementados

--- Prototype 01/Assets/Scripts/PlayerController.cs
+++ Prototype 01/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // Variables
+    public float speed = 5.0f;
+    public float turnSpeed;
+    public float horizontalInput;
+    public float forwardInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +19,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
         // Movemos el vehiculo hacia delante
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
